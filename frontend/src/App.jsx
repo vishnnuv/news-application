46d25076c6b36d2e848cb6ade26b216cd5e3c0d8
Cenote <himangshukalita131@gmail.com>
2024-11-15 13:02:50 +0530

    Installing shadcn UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react"
+import { Button } from "./components/ui/button"
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+
+      <Button className="bg-red-500" variant="ghost">
+        Click me
+      </Button>
+    </div>
   )
 }
 
