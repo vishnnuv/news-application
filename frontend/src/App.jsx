edfb144c83bd0fbb121d61f82f0dbd3809f30b37
Cenote <himangshukalita131@gmail.com>
2024-11-16 11:12:28 +0530

    create footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from "./pages/Dashboard"
 import NewsArticles from "./pages/NewsArticles"
 import Header from "./components/shared/Header"
 import { Toaster } from "./components/ui/toaster"
+import Footer from "./components/shared/Footer"
 
 const App = () => {
   return (
@@ -22,6 +23,8 @@ const App = () => {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/news" element={<NewsArticles />} />
       </Routes>
+
+      <Footer />
 
       <Toaster />
     </BrowserRouter>
