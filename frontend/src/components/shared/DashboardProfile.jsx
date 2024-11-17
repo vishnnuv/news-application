b523586284a9ecfc8a7a952882cf295843646c78
Cenote <himangshukalita131@gmail.com>
2024-11-17 12:35:25 +0530

    add image upload functionality using appwrite

--- frontend/src/components/shared/DashboardProfile.jsx
+++ frontend/src/components/shared/DashboardProfile.jsx
@@ -1,10 +1,25 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 import { useSelector } from "react-redux"
 import { Input } from "../ui/input"
 import { Button } from "../ui/button"
 
 const DashboardProfile = () => {
   const { currentUser } = useSelector((state) => state.user)
+
+  const profilePicRef = useRef()
+
+  const [imageFile, setImageFile] = useState(null)
+  const [imageFileUrl, setImageFileUrl] = useState(null)
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0]
+
+    // console.log(file)
+    if (file) {
+      setImageFile(file)
+      setImageFileUrl(URL.createObjectURL(file))
+    }
+  }
 
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
@@ -13,11 +28,20 @@ const DashboardProfile = () => {
       </h1>
 
       <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          accept="image/*"
+          hidden
+          ref={profilePicRef}
+          onChange={handleImageChange}
+        />
+
         <div className="w-32 h-32 self-center cursor-pointer overflow-hidden">
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt=""
             className="rounded-full w-full h-full object-cover border-8 border-gray-300"
+            onClick={() => profilePicRef.current.click()}
           />
         </div>
 
