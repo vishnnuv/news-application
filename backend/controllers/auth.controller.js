3a16953cba3fae31491cbfd4f7d87d55a3b7aa8b
Cenote <himangshukalita131@gmail.com>
2024-11-19 14:21:14 +0530

    admin field added to user model

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -54,7 +54,10 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, "Wrong Credentials"))
     }
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    )
 
     const { password: pass, ...rest } = validUser._doc
 
@@ -71,7 +74,10 @@ export const google = async (req, res, next) => {
     const user = await User.findOne({ email })
 
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      )
 
       const { password: pass, ...rest } = user._doc
 
@@ -101,7 +107,10 @@ export const google = async (req, res, next) => {
 
     await newUser.save()
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET)
+    const token = jwt.sign(
+      { id: newUser._id, isAdmin: newUser.isAdmin },
+      process.env.JWT_SECRET
+    )
 
     const { password: pass, ...rest } = newUser._doc
 
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "https://cdn-icons-png.flaticon.com/128/3177/3177440.png",
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 )
