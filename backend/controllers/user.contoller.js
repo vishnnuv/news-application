350d911d0c97353c55f122c5a84d589e2dd84df5
Cenote <himangshukalita131@gmail.com>
2024-11-21 14:25:13 +0530

    get users api route

--- backend/controllers/user.contoller.js
+++ backend/controllers/user.contoller.js
@@ -83,3 +83,50 @@ export const signout = async (req, res, next) => {
     next(error)
   }
 }
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(
+      errorHandler(403, "You are not authorized to access this resource!")
+    )
+  }
+
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0
+    const limit = parseInt(req.query.limit) || 9
+    const sortDirection = req.query.sort === "asc" ? 1 : -1
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit)
+
+    const getUsersWithoutPassword = users.map((user) => {
+      const { password: pass, ...rest } = user._doc
+
+      return user
+    })
+
+    const totalUsers = await User.countDocuments()
+
+    const now = new Date() // 2024 15 Nov
+
+    const oneMonthAgo = new Date( // 2024 15 Oct
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    )
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    })
+
+    res.status(200).json({
+      users: getUsersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
