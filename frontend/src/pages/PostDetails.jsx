6e5fa289f23310bdb8c2179ac9cb6687d9802d85
Cenote <himangshukalita131@gmail.com>
2024-11-23 12:02:40 +0530

    Recent Article section in the post details page

--- frontend/src/pages/PostDetails.jsx
+++ frontend/src/pages/PostDetails.jsx
@@ -1,5 +1,6 @@
 import Advertise from "@/components/shared/Advertise"
 import CommentSection from "@/components/shared/CommentSection"
+import PostCard from "@/components/shared/PostCard"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import React, { useEffect, useState } from "react"
@@ -11,6 +12,9 @@ const PostDetails = () => {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
   const [post, setPost] = useState(null)
+  const [recentArticles, setRecentArticles] = useState(null)
+
+  console.log(recentArticles)
 
   // console.log(post)
 
@@ -43,6 +47,24 @@ const PostDetails = () => {
 
     fetchPost()
   }, [postSlug])
+
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`)
+
+        const data = await res.json()
+
+        if (res.ok) {
+          setRecentArticles(data.posts)
+        }
+      }
+
+      fetchRecentPosts()
+    } catch (error) {
+      console.log(error.message)
+    }
+  }, [])
 
   if (loading) {
     return (
@@ -97,6 +119,19 @@ const PostDetails = () => {
       </div>
 
       <CommentSection postId={post._id} />
+
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl font-semibold mt-5 text-slate-700">
+          Recently published articles
+        </h1>
+
+        <div className="flex flex-wrap gap-5 my-5 justify-center">
+          {recentArticles &&
+            recentArticles.map((post) => (
+              <PostCard key={post._id} post={post} />
+            ))}
+        </div>
+      </div>
     </main>
   )
 }
